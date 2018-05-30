7f87281dc66e408408f1b74072c1a87d766d8ca8
sgutierrez <kei.miskatonic@gmail.com>
2018-05-30 12:36:38 -0400

    Implementación Login

--- app/src/main/java/com/example/pablorjd/CheckThisOut/Login.java
+++ app/src/main/java/com/example/pablorjd/CheckThisOut/Login.java
@@ -2,12 +2,13 @@ package com.example.pablorjd.CheckThisOut;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
-    EditText etEmail;
+    EditText etUsername;
     EditText etPassword;
     Button btnLogin;
 
@@ -16,5 +17,19 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = (EditText)findViewById(R.id.etUsername);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+    }
+
+    public void onLogin(View view){
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type,username,password);
+
     }
 }

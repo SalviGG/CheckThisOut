7b6d7c0b6baa66fb5961d073154229b2abaf5a05
SalviGG <kei.miskatonic@gmail.com>
2018-06-06 16:20:35 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+﻿<?php
+require "conn.php";
+$user_name = $_POST["user_name"];
+$user_password = $_POST["password"];
+$mysql_query = "select * from users where username like '$user_name' and password like '$user_password';";
+$result = mysqli_query($conn, $mysql_query);
+if(mysqli_num_rows($result)>0){
+	$result = "true";
+}
+else{
+	$result = "false";
+}
+echo $result;
+?>

6f5b571d0f68fb6c00bd0c55175e1dcbfacdb7a8
SalviGG <kei.miskatonic@gmail.com>
2018-06-06 16:20:35 -0400

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,7 @@
+﻿<?php
+$db_name = "checkthisout";
+$mysql_username = "root";
+$mysql_password = "";
+$server_name = "localhost";
+$conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
+?>
